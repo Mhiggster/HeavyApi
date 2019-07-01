7b3c303918bb17631f3c118fab2ab158fdb7ef22
mhiggster <mhiggster@gmail.com>
2019-07-01 09:07:57 +0600

    draft thing

---
+++ src/Acme/Log.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Acme;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\FirePHPHandler;
+
+class Log
+{
+    public function write($message, $level)
+    {
+        
+    }
+}
